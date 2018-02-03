75573394ceb2436fc35182874957d37f08c4f3d1
Chris Roscher <chris@aitai.nl>
2018-02-03 15:47:40 +0100

    Menubalk toegevoegd

--- Parkeersimulator/main/Garage.java
+++ Parkeersimulator/main/Garage.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import model.Simulator;
 import view.CarGraph;
@@ -25,12 +27,23 @@ public class Garage {
 	public Garage() {
 		// generate random numbers for testing purposes
 		List<Integer> list = new ArrayList<Integer>();
-	      Random random = new Random();
-	      int maxDataPoints = 24;
-	      int maxScore = 20;
-	      for (int i = 0; i < maxDataPoints ; i++) {
-	         list.add(random.nextInt(maxScore));
-	      }
+		Random random = new Random();
+		int maxDataPoints = 24;
+		int maxScore = 20;
+		for (int i = 0; i < maxDataPoints; i++) {
+			list.add(random.nextInt(maxScore));
+		}
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Opties");
+		JMenuItem settingsItem = new JMenuItem("Instellingen");
+
+		settingsItem.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("setting test");
+			}
+		});
 
 		simulator = new Simulator();
 		garageView = new GarageView(simulator);
@@ -38,37 +51,32 @@ public class Garage {
 		garageController = new GarageController(simulator);
 		info = new InfoView(simulator);
 		window = new JFrame("Parkeergarage simulatie");
-		
+
 		window.setSize(1200, 700);
 		window.setResizable(false);
 		window.setLayout(null);
-		window.getContentPane().add(garageView);
-		window.getContentPane().add(info);
-		window.getContentPane().add(garageController);
-		window.getContentPane().add(graph);
-		
-		garageView.setBounds(-50,20,800,300);
-		garageController.setBounds(10,320,730,30);
-		garageController.setBackground(Color.red);
-		info.setBounds(750,10,430,400);
-		info.setBackground(Color.black);
-		graph.setBounds(10,360,400,300);
-		graph.setBackground(Color.lightGray);
 
-//		Container garageAndButtons = window.getContentPane();
-//		Container infoPane = window.getContentPane();
-//		Container graphPane = window.getContentPane();
-//		graphPane.setLayout(new BorderLayout());
-//		garageAndButtons.add(garageController, BorderLayout.SOUTH);
-//		garageAndButtons.add(garageView, BorderLayout.CENTER);
-//		infoPane.add(info, BorderLayout.EAST);
-//		graphPane.add(graph,BorderLayout.LINE_START);
-//
-//		window.pack();
+		window.add(garageView);
+		window.add(info);
+		window.add(garageController);
+		window.add(graph);
+		window.setJMenuBar(menuBar);
+		menuBar.add(menu);
+		menu.add(settingsItem);
+
+		garageView.setBounds(-50, 20, 800, 300);
+		garageController.setBounds(10, 320, 730, 30);
+		garageController.setBackground(Color.red);
+		info.setBounds(750, 10, 430, 400);
+		info.setBackground(Color.black);
+		graph.setBounds(10, 360, 400, 300);
+		graph.setBackground(Color.lightGray);
+		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		simulator.tick();
 	}
+
 	public static void main(String[] args) {
 		new Garage();
 	}
