e0db9d3f894f53343ea81381e4e303c20fa628c3
Kevin Jersak <KevinJersak@hotmail.com>
2018-02-05 17:10:22 +0100

    Javadocs toegevoegd aan de klassen AdHocCar, ParkingPassCar en ReservedCar

--- Parkeersimulator/model/AdHocCar.java
+++ Parkeersimulator/model/AdHocCar.java
@@ -2,7 +2,9 @@ package model;
 
 import java.awt.*;
 import java.util.Random;
+
 /**
+ * Een klasse voor ad hoc auto's met de kleur rood
  * 
  * @author MinuteJ
  * 1.0.0
@@ -11,17 +13,27 @@ public class AdHocCar extends Car {
     private static final Color COLOR = Color.red;
     private final int stayMinutes;
 
+    /**
+     * Creër een nieuwe ad hoc auto, bepaal hoelang deze blijft
+     * en of hij moet betalen
+     */
     public AdHocCar() {
         Random random = new Random();
         this.stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
         this.setMinutesLeft(stayMinutes);
         this.setHasToPay(true);
     }
-
+    
+    /**
+     * @return geeft de parkeer tijd in minuten
+     */
     public int getParkingTime() {
         return stayMinutes;
     }
 
+    /**
+     * @return geeft de kleur van de auto
+     */
     public Color getColor() {
         return COLOR;
     }
--- Parkeersimulator/model/ParkingPassCar.java
+++ Parkeersimulator/model/ParkingPassCar.java
@@ -1,15 +1,21 @@
 package model;
+
+import java.awt.*;
+import java.util.Random;
+
 /**
+ * Een klasse voor abonnement auto's met de kleur blauw
  * 
  * @author MinuteJ
  * @version 1.0.0
  */
-import java.awt.*;
-import java.util.Random;
-
 public class ParkingPassCar extends Car {
     private static final Color COLOR = Color.blue;
 
+    /**
+     * Creër een nieuwe abonnement auto, bepaal hoelang deze blijft
+     * en of hij moet betalen
+     */
     public ParkingPassCar() {
         Random random = new Random();
         int stayMinutes = (int) (15 + random.nextFloat() * 3 * 60);
@@ -17,6 +23,9 @@ public class ParkingPassCar extends Car {
         this.setHasToPay(false);
     }
 
+    /**
+     * @return geeft de kleur van de auto
+     */
     public Color getColor() {
         return COLOR;
     }
