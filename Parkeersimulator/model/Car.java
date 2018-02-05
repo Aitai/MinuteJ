f5c1d25536b9d385b745794a5dc2b6dd38d98680
Yaimon Amatbasoeki <yaimon@live.nl>
2018-02-05 18:06:54 +0100

    Javadoc bij car en carqueue

--- Parkeersimulator/model/Car.java
+++ Parkeersimulator/model/Car.java
@@ -21,42 +21,83 @@ public abstract class Car {
 
     }
 
-    
+    /**
+     * Geef de locatie terug
+     * @return de locatie
+     */
     public Location getLocation() {
         return location;
     }
-
+    
+    /**
+     * Pas de locatie aan
+     * 
+     * @param location De locatie van de auto
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Krijg de minuten die de auto nog heeft
+     * @return de overige minuten
+     */
     public int getMinutesLeft() {
         return minutesLeft;
     }
 
+    /**
+     * Pas de overige minuten aan
+     * 
+     * @param minutesLeft de minuten die nog over zijn
+     */
     void setMinutesLeft(int minutesLeft) {
         this.minutesLeft = minutesLeft;
     }
 
+    /**
+     * Kijk of de auto aan het betalen is of niet
+     * @return true als de auto aan het betalen is
+     */
     public boolean getIsPaying() {
         return isPaying;
     }
 
+    /**
+     * Pas aan of de auto aan het betalen is of niet
+     * 
+     * @param isPaying true als de de auto aan het betalen is
+     */
     public void setIsPaying(boolean isPaying) {
         this.isPaying = isPaying;
     }
 
+    /**
+     * Kijk of de auto nog moet betalen of niet
+     * @return true als de auto nog moet betalen
+     */
     public boolean getHasToPay() {
         return hasToPay;
     }
 
+    /**
+     * Pas aan of de auto nog moet betalen of niet
+     * @param hasToPay true als de auto nog moet betalen
+     */
     void setHasToPay(boolean hasToPay) {
         this.hasToPay = hasToPay;
     }
 
+    /**
+     * Haalt de overige minuten naar beneden
+     */
     public void tick() {
         minutesLeft--;
     }
 
+    /**
+     * Abstracte methode om de kleur te krijgen
+     * @return de kleur
+     */
     public abstract Color getColor();
 }
--- Parkeersimulator/model/CarQueue.java
+++ Parkeersimulator/model/CarQueue.java
@@ -10,18 +10,34 @@ import java.util.Queue;
 class CarQueue {
     private final Queue<Car> queue = new LinkedList<>();
 
+    /**
+     * Voege een auto toe
+     * @param car de auto die toegevoegd gaat worden
+     */
     public void addCar(Car car) {
         queue.add(car);
     }
 
+    /**
+     * Haal een auto uit de rij
+     * @return de auto die eruit is gehaald
+     */
     public Car removeCar() {
         return queue.poll();
     }
 
+    /**
+     * Geef de volgende auto in de rij
+     * @return de volgende auto in de rij
+     */
     Car nextCar() {
         return queue.peek();
     }
 
+    /**
+     * Geef de grootte van de rij
+     * @return de rij van auto's
+     */
     public int carsInQueue() {
         return queue.size();
     }
